e59e8bc678eb108e970c7ea861296af08d4855e3
hyein <junghi0327@gmail.com>
2020-01-05 22:17:18 +0900

    lpay 수정

--- web/lpay/js/ui.js
+++ web/lpay/js/ui.js
@@ -1,12 +1,3 @@
-// ie foreach
-if (!Array.prototype.forEach) {
-      Array.prototype.forEach = function (fn, scope) {
-      for (var i = 0, len = this.length; i < len; ++i) {
-      fn.call(scope || this, this[i], i, this);
-    }
-  }
-}
-
 const ui = () => {
   uiHeader();
   uiTooltip();
